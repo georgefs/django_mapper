991a19e97bbe77548d1e646a7c422c1ee9e6a4e4
george <goblin_george@gmail.com>
2013-04-14 07:24:25 +0800

    set install contain template files

--- setup.py
+++ setup.py
@@ -4,6 +4,37 @@ try:
 except:
     from distutils.core import setup, find_packages
 import django_mapper as distmeta
+import os
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+django_dir = 'django_mapper'
+
+for dirpath, dirnames, filenames in os.walk(django_dir):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+print data_files
 
 setup(
     version=distmeta.__version__,
@@ -13,5 +44,7 @@ setup(
     url=distmeta.__homepage__,
     #
     name='django_mapper',
-    packages=find_packages(),
+    packages=('django_mapper',),
+    data_files= data_files,
+    include_package_data=True,
 )
